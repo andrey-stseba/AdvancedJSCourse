b48511d261976ad3c15235acc11fb83333053ee3
andrey <scebaandru@gmail.com>
2022-06-25 11:53:22 +0300

    practice lesson 2

--- lesson_2/js/app.js
+++ lesson_2/js/app.js
@@ -12,3 +12,19 @@ console.log('b', b)
 const c = 5
 console.log('c', c)
 // c = 6 // Error
+
+d = 5
+d = 6
+var d
+console.log('d', d)
+
+let name = 'Bob',
+  age = 18,
+  gender = 'male'
+
+console.log(`Name: ${name} age: ${age} gender: ${gender}`)
+
+const email = 'aaaa@aaaaa.com',
+  password = '1hiwhfi1'
+
+console.log('email, password', email, password)
