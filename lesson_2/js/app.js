acf077fef436afc33d87cf810ebd17c2dd8828d3
andrey <scebaandru@gmail.com>
2022-06-25 11:32:34 +0300

    practice lesson 2

---
+++ lesson_2/js/app.js
@@ -0,0 +1,14 @@
+var a = 1
+console.log('a', a)
+var a = 'Hello'
+console.log('a', a)
+let b = 3
+// let b = 5 // Error
+// const b = 5; // Error
+console.log('b', b)
+b = 4
+console.log('b', b)
+
+const c = 5
+console.log('c', c)
+// c = 6 // Error
